cd93c123a4cce65660dd4c4b4ce6be6fd9da6a6a
Nygil Pereira <nygilgp@gmail.com>
2019-08-09 02:58:52 +0530

    * CitiesDataStore.cs: Adding children items
    * CityDto.cs:
    * PointOfInterestDto.cs:
    * CitiesController.cs:
    * PointOfIntrestController.cs:

--- CityInfo.API/CitiesDataStore.cs
+++ CityInfo.API/CitiesDataStore.cs
@@ -4,38 +4,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Cityinfo.API
+namespace CityInfo.API
 {
     public class CitiesDataStore
     {
-        // Added using the lates syntax of C#, which will help to keep
-        // the current state of city data store intact till refreshed
         public static CitiesDataStore Current { get; } = new CitiesDataStore();
         public List<CityDto> Cities { get; set; }
 
         public CitiesDataStore()
         {
+            // init dummy data
             Cities = new List<CityDto>()
             {
                 new CityDto()
                 {
-                    Id = 1,
-                    Name = "Delhi",
-                    Description = "Capital city"
+                     Id = 1,
+                     Name = "New York City",
+                     Description = "The one with that big park.",
+                     PointsOfInterest = new List<PointOfInterestDto>()
+                     {
+                         new PointOfInterestDto() {
+                             Id = 1,
+                             Name = "Central Park",
+                             Description = "The most visited urban park in the United States." },
+                          new PointOfInterestDto() {
+                             Id = 2,
+                             Name = "Empire State Building",
+                             Description = "A 102-story skyscraper located in Midtown Manhattan." },
+                     }
                 },
                 new CityDto()
                 {
                     Id = 2,
-                    Name = "Trivandrum",
-                    Description = "Capital city of Kerala"
+                    Name = "Antwerp",
+                    Description = "The one with the cathedral that was never really finished.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                     {
+                         new PointOfInterestDto() {
+                             Id = 3,
+                             Name = "Cathedral of Our Lady",
+                             Description = "A Gothic style cathedral, conceived by architects Jan and Pieter Appelmans." },
+                          new PointOfInterestDto() {
+                             Id = 4,
+                             Name = "Antwerp Central Station",
+                             Description = "The the finest example of railway architecture in Belgium." },
+                     }
                 },
                 new CityDto()
                 {
-                    Id = 3,
-                    Name = "Chennai",
-                    Description = "Capital city of TN"
+                    Id= 3,
+                    Name = "Paris",
+                    Description = "The one with that big tower.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                     {
+                         new PointOfInterestDto() {
+                             Id = 5,
+                             Name = "Eiffel Tower",
+                             Description = "A wrought iron lattice tower on the Champ de Mars, named after engineer Gustave Eiffel." },
+                          new PointOfInterestDto() {
+                             Id = 6,
+                             Name = "The Louvre",
+                             Description = "The world's largest museum." },
+                     }
                 }
             };
+
         }
     }
 }
