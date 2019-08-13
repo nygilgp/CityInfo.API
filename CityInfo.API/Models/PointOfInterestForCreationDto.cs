1887ff728d58ac543473f92472920113f9a7347e
nygilgp <nygilgp@gmail.com>
2019-08-13 14:29:34 +0530

    Create PointOfInterestForCreationDto.cs
    ID is not controlled by the api when point of interest is created, it should be provided by DB

---
+++ CityInfo.API/Models/PointOfInterestForCreationDto.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cityinfo.API.Models
+{
+    public class PointOfInterestForCreationDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
