a7def39a5ad06595657a24b56bebeb466de524fe
nygilgp <nygilgp@gmail.com>
2019-08-14 19:35:52 +0530

    Adding NLog to logger factory

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
+using NLog.Extensions.Logging;
 
 namespace CityInfo.API
 {
@@ -38,6 +39,9 @@ namespace CityInfo.API
             loggerFactory.AddConsole();
 
             loggerFactory.AddDebug();
+
+            // loggerFactory.AddProvider(new NLog.Extensions.Logging.NLogLoggerProvider());
+            loggerFactory.AddNLog();
 
             if (env.IsDevelopment())
             {
